e48116b5b902b4838b3ed7fd3321b699db6f5161
WiemerSteenbeek <wiemer.steenbeek@gmail.com>
2023-09-13 19:23:34 +0200

    Starter project done

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
+import { MendixPlatformClient, setPlatformConfig } from "mendixplatformsdk";
+import * as fs from "fs";
+
 const config = require("../config/config.json");
 
 main().catch(console.error);
 
 async function main() {
-  console.log("Hello World");
-  console.log(config.mendixtoken);
+  const mxClient = new MendixPlatformClient();
+  setPlatformConfig({ mendixToken: config.mendixtoken });
+  const mxApp = mxClient.getApp(config.projectid);
+  const branch = await mxApp.getRepository().getBranch("main");
+  const mxWorkingCopy = await mxApp.createTemporaryWorkingCopy(branch.name);
+  const mxModel = await mxWorkingCopy.openModel();
 }
