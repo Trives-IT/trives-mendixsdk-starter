dc2c1de7eea5829316a186de37599d7bd020aaf2
WiemerSteenbeek <wiemer.steenbeek@gmail.com>
2023-09-13 19:42:20 +0200

    Basic project done

---
+++ readme.md
@@ -0,0 +1,16 @@
+Setup:
+
+- Follow the Mendix tutorial on how to setup a Typescript project
+
+- Create a folder named 'config' in this project's root folder
+- In it, put a file named 'config.json' that contains the following JSON snippet:
+
+{
+"projectid": "44ad0bb8-dce0-4a1d-b123-123abd871f7f",
+"mendixtoken": "4g442gtMzKtKxQy8aKFnFfVg7Kt54HuWAK1bSSSswfzsdhsUZ6DFGatXW6XHCQyG3RLzraHjhiAi3dh7QkoaGuKdSstxS7okLRGy",
+"branch": "main"
+}
+
+- Replace the projectid with the ID of the project you want to use
+- Replace the mendixtoken with the one you generated
+- (Optional) Replace the branch name if you don't want to use the main branch of your project
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
 import { MendixPlatformClient, setPlatformConfig } from "mendixplatformsdk";
 import * as fs from "fs";
+import { IModel } from "mendixmodelsdk";
 
 const config = require("../config/config.json");
 
 main().catch(console.error);
 
 async function main() {
-  const mxClient = new MendixPlatformClient();
+  // Set up your Mendix Model SDK client:
   setPlatformConfig({ mendixToken: config.mendixtoken });
+  const mxClient = new MendixPlatformClient();
   const mxApp = mxClient.getApp(config.projectid);
-  const branch = await mxApp.getRepository().getBranch("main");
+  const branch = await mxApp.getRepository().getBranch(config.branch);
   const mxWorkingCopy = await mxApp.createTemporaryWorkingCopy(branch.name);
   const mxModel = await mxWorkingCopy.openModel();
+
+  // Do your worst here :-):
+  console.log("Your Mendix version is: " + mxModel.mxVersionForModel);
 }
